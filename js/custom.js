964a1b589058b38483d4de7e15e99301f42e56cc
Your Name <you@example.com>
2021-06-04 04:48:29 -0600

    working on active menu state

--- js/custom.js
+++ js/custom.js
@@ -155,7 +155,7 @@
         if ($('.fullpage-default').length) {
             var myFullpage = new fullpage('.fullpage-default', {
                 licenseKey: ' C7F41B00-5E824594-9A5EFB99-B556A3D5',
-                anchors: ['slide000', 'slide001', 'slide002', 'slide003', 'slide004', 'slide005', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
+                anchors: ['slide000', 'slide001', 'slide002', 'slide003', 'slide004', 'slide005', 'slide006', 'slide02', 'slide03', 'slide04', 'slide05', 'slide06', 'slide07'],
                 menu: '#nav',
                 lazyLoad: true,
                 navigation: true,
@@ -166,6 +166,11 @@
                 responsiveSlides: true
             });
         }
+        
+        // Navigation Active Functionality
+        
+        
+        
         $(document).on('click', '.navbar-toggle', function() {
             $('.navbar-collapse').slideToggle(300);
             return false;
